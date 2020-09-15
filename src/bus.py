38a70ca42e2b0e9e487d9973759b59851df4a4a7
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-15 15:25:08 +0100

    added property self.passengers to bus class and created get passengers function and created person class" "

--- src/bus.py
+++ src/bus.py
@@ -2,6 +2,11 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = []
 
     def drive(self):
         return("Vroom vroom")
+
+    def get_passengers_on_bus(self):
+        return len(self.passengers)
+        
