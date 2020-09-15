cf22e6b11df5d52ccc394c08ec0cdb67c0c02ac1
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-15 16:14:40 +0100

    added pick_up_from_stop method and passed final test

--- src/bus.py
+++ src/bus.py
@@ -19,4 +19,8 @@ class Bus:
     def empty(self):
         self.passengers.clear()
 
-    
+    def pick_up_from_stop(self, bus_stop):
+        for person in bus_stop.queue:
+            self.pick_up(person)
+        
+
